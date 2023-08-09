93c545ee1d7d3d72f6934ab566a41769dfa2dc66
Troy Lopez <troy.n.lopez@gmail.com>
2023-08-08 19:09:48 -0700

    generate markdown

---
+++ generate/md_gen.py
@@ -0,0 +1,55 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+class Markdown:
+    def __init__(self, filename):
+        self.filename = filename + ".md"
+        self.doc = []
+    
+    def __str__(self):
+        return "".join(self.doc)
+    
+    def print(self):
+        print(str(self))
+
+    def paragraph(self, text):
+        self.doc.append(text)
+        self.doc.append("\n")
+
+    def title(self, text):
+        self.doc.append(f"# {text}\n\n")
+
+    def heading(self, text):
+        self.doc.append(f"## {text}\n\n")
+
+    def subheading(self, text):
+        self.doc.append(f"### {text}\n\n")
+
+    def subsubheading(self, text):
+        self.doc.append(f"#### {text}\n\n")
+
+    def horizontal_rule(self):
+        self.doc.append("-----\n\n")
+
+    def code_block(self, text):
+        self.doc.append(f"```\n{text}\n```\n\n")
+
+    def bullet_list(self, items):
+        for item in items:
+            self.doc.append(f"* {item}\n")
+        self.doc.append("\n\n")
+
+    def numbered_list(self, items):
+        index = 1
+        for item in items:
+            self.doc.append(f"{index}. {item}\n")
+            index += 1
+        self.doc.append("\n\n")
+
+    def new_line(self):
+        self.doc.append("\n\n")
+
+    def save_and_close(self):
+        with open(self.filename, "w") as f:
+            f.write(str(self))
