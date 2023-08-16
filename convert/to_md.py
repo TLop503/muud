02aece34891e5def96b1ce4f82219b43d4b109aa
Troy Lopez <troy.n.lopez@gmail.com>
2023-08-16 15:12:03 -0700

    implement muudi to md, needs testing

---
+++ convert/to_md.py
@@ -0,0 +1,53 @@
+import logging
+import json
+
+import generate.md_gen as md_gen
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG)
+
+
+data = None
+
+
+class Converter:
+    def convert(file_path):
+        # instatiate the markdown generator:
+        md = md_gen.Markdown()
+
+        # open and read the document:
+        with open(file_path, "r") as f:
+            data = json.load(f)
+
+        # convert the document:
+        for item in data:
+            """The key is the type of element, the value is the content of the element.
+            Translations for keys can be found in muudiJson.py. in the top level of this repo."""
+            for key, value in item.items():
+                if key == ";pa":
+                    md.paragraph(value)
+                elif key == ";ti":
+                    md.title(value)
+                elif key == ";he":
+                    md.heading(value)
+                elif key == ";sh":
+                    md.subheading(value)
+                elif key == ";ssh":
+                    md.subsubheading(value)
+                elif key == ";hr":
+                    md.horizontal_rule()
+                elif key == ";cb":
+                    md.code_block(value)
+                elif key == ";bl":
+                    md.bullet_list(value)
+                elif key == ";nl":
+                    md.numbered_list(value)
+                elif key == ";nu":
+                    md.new_line()
+                else:
+                    logger.error(f"Unknown key: {key} with value: {value}")
+        md.save_and_close()
+
+
+if __name__ == "__main__":
+    Converter.convert("../test.json")
